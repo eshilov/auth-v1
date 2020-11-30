913f7958288bb91c4ed60787cebb3bfd04f36512
Evgeny Shilov <evgeny.shilov225@gmail.com>
2020-12-01 01:16:17 +0300

    Add spring exception handling controller advice

---
+++ src/main/java/com/eshilov/auth/ExceptionHandlingAdvice.java
@@ -0,0 +1,16 @@
+package com.eshilov.auth;
+
+import static org.springframework.http.ResponseEntity.badRequest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlingAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException exception) {
+        return badRequest().body(exception.getMessage());
+    }
+}
