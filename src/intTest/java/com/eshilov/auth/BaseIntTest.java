0e6abfe70043a7ca5ec37991d910d1f13a85c077
Evgeny Shilov <evgeny.shilov225@gmail.com>
2020-11-30 19:54:36 +0300

    Add base integration test

---
+++ src/intTest/java/com/eshilov/auth/BaseIntTest.java
@@ -0,0 +1,22 @@
+package com.eshilov.auth;
+
+import static lombok.AccessLevel.PROTECTED;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Getter(PROTECTED)
+@AutoConfigureMockMvc
+@Accessors(fluent = true)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class BaseIntTest {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+}
